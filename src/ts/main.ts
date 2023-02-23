d5528482996fa1affe22d856ca1c4aba25cde585
Hanna Forssell <forssellhanna@gmail.com>
2023-02-23 10:33:48 +0100

    Fixed file structure

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -5,6 +5,8 @@ import Splide from '@splidejs/splide';
 var splide = new Splide( '.splide', {
     type   : 'loop',
     padding: '5rem',
-  } );
+});
   
-  splide.mount();
+splide.mount();
+
+console.log('gör någonting i main');
