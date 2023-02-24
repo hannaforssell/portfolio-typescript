eb1e80af06bd86d5b8744930832dbe0cb922dff6
Hanna Forssell <forssellhanna@gmail.com>
2023-02-24 14:26:15 +0100

    Adds slideshow with placeholder images

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,10 @@
-import './scss/style.scss'
+import '../scss/style.scss'
+
+import Splide from '@splidejs/splide';
+
+var splide = new Splide( '.splide', {
+    type   : 'loop',
+    padding: '5rem',
+  } );
+  
+  splide.mount();
